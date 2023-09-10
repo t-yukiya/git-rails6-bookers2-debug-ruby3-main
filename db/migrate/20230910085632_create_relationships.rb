3737f8abc3bf6b16b392946687b535a6997392df
EC2 Default User <141203907+t-yukiya@users.noreply.github.com>
2023-09-10 09:07:15 +0000

    [Break]3-4

--- db/migrate/20230910085632_create_relationships.rb
+++ db/migrate/20230910085632_create_relationships.rb
@@ -1,8 +1,9 @@
 class CreateRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :relationships do |t|
-      t.integer :follower_id
+
       t.integer :followed_id
+      t.integer :follower_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_10_061400) do
+ActiveRecord::Schema.define(version: 2023_09_10_085632) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -64,8 +64,8 @@ ActiveRecord::Schema.define(version: 2023_09_10_061400) do
   end
 
   create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id"
     t.integer "followed_id"
+    t.integer "follower_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
